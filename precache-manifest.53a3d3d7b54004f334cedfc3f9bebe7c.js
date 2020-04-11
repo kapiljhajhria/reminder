4c44d32d213db312885d27972f9fc682dc0aa4d1
kapil jhajhria <jhajhria44@gmail.com>
2020-04-11 08:22:23 +0530

    Updates

---
+++ precache-manifest.53a3d3d7b54004f334cedfc3f9bebe7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbb4433d183d4a53a727c72aac64e1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac2a430868db896d6f2",
+    "url": "/static/css/main.fd08d510.chunk.css"
+  },
+  {
+    "revision": "7d078273798aed9017c0",
+    "url": "/static/js/2.85dc6b53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85dc6b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac2a430868db896d6f2",
+    "url": "/static/js/main.b216fdf6.chunk.js"
+  },
+  {
+    "revision": "12d6d768069edc542eb0",
+    "url": "/static/js/runtime-main.11f4151b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
