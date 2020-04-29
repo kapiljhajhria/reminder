b08a55d9e7cfe45c5797ae5738a32759f7c49628
kapil jhajhria <jhajhria44@gmail.com>
2020-04-29 19:58:23 +0530

    using functions now, code refactored. able to add timers with countdown now.
    todo: clear timers on end

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,81 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles.css'
 import Timer from "../Timer";
 
-class GetInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            datetime: "12/12/1212",
+export default function GetInput(props) {
+    const [endTime, setEndTime] = useState("");
+    const [endTimesList, setEndTimesList] = useState([]);
 
-            endTimeList: [],
-
-        }
-    }
-
-    onBtnPress() {
-        // console.log(this.state.datetime)
-        // console.log("above this is the time from input field, end time of timer")
-        // console.log(new Date(this.state.datetime).getTime())
-        let endTimeListCopy = [].concat(this.state.divList);
-        endTimeListCopy.push(this.state.datetime);
-        return (
-            this.setState({
-                endTimeList: endTimeListCopy,
-            })
-        );
+    let tempEndTime;
 
 
-    }
-
-    // endTimeFromInput
-
-    async removeElementFromList() {
-        console.log("filtering list")
-        let endTimeListCopy = [].concat(this.state.endTimeList);
-        endTimeListCopy = endTimeListCopy.filter((endTime) => (new Date(endTime).getTime() - Date.now()) > 0);
-
-        this.setState({
-            endTimeList: endTimeListCopy,
-        })
-        alert('Timer Completed, Its ' + new Date(Date.now()).toLocaleTimeString() + " now");
-    }
-
-    isFutureTimer(endTime) {
-        let deadline = new Date(endTime).getTime();
-        let timeNow = new Date().getTime();
-        return deadline - timeNow > 0;
-    }
-
-    getTimersDivList() {
-        return this.state.endTimeList.map((endTime) => this.isFutureTimer(endTime) ?
-            <div className={"rem"}>
-                <div>
-                    <Timer key={endTime} dateTime={endTime}
-                           removeElementFromList={() => this.removeElementFromList()}/>
-                </div>
-            </div> : ""
-        )
-    }
-
-    render() {
-        return (
+    return (
+        <div className="add-reminder-cont">
             <div>
-                <div className={"all-el"}>
-                    <input className="datetimepicker" type="datetime-local" onChange={(newDateEvent) => {
-                        this.setState({datetime: newDateEvent.target.value});
-                    }}/>
-
-                    <button className={"add-rem-btn"}
-                            onClick={() => this.onBtnPress()}
-                    >
-                        Add Reminder
-                    </button>
-                </div>
-                {this.getTimersDivList()}
+                <input type="datetime-local" onChange={(dateTimeEntered) => {
+                    setEndTime(dateTimeEntered.target.value);
+                }}/>
             </div>
-        );
-    }
+            <div>
+                <button onClick={() => {
+                    let tempEndTimeList = [].concat(endTimesList)
+                    tempEndTimeList.push(endTime)
+                    setEndTimesList(tempEndTimeList);
+                }}>
+                    Set Reminder
+                </button>
+            </div>
+            <div className="timersListCol">
+                {endTimesList.map((t) => (
+                    <Timer key={t} endTimeInMili={t}>
+                        <div>
+                            {t}</div>
+                    </Timer>
+                ))
+                }
+            </div>
+        </div>
+    )
 }
-
-export default GetInput;
--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,59 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './styles.css'
 
-class Timer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fromTime: this.props.dateTime,
-            countdown: 'Starting Timer',
+export default function Timer(props) {
+    const [endTimeInMilliseconds, setEndTimeInMilliseconds] = useState(new Date(props.endTimeInMili).getTime());
+    const [countdown, setCountDown] = useState("");
+    let intervalId;
 
-        };
-        this.intervalId=null;
-    }
-    startTimer (stringTime)  {
-
-        console.log('string is :' + stringTime);
-        let deadline = new Date(stringTime).getTime();
-
-        this.intervalId  = setInterval(() => {
-            let now = new Date().getTime();
-            let t = deadline - now;
-            let days = Math.floor(t / (1000 * 60 * 60 * 24));
-            let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-            let seconds = Math.floor((t % (1000 * 60)) / 1000);
-            if (t<=0) {
-                console.log("timer ended")
-                clearInterval(this.intervalId);
-                this.props.removeElementFromList();
-                return 0;
-            }else
-            this.setState({
-                countdown: days + "d "
-                    + hours + "h " + minutes + "m " + seconds + "s ",
-            });
-
-        }, 1000);
+    function getCountDownFromMilliseconds(endTime) {
+        endTime = endTime - new Date().getTime();
+        let seconds = Math.floor((endTime / 1000) % 60)
+        let minutes = Math.floor((endTime / (1000 * 60)) % 60)
+        let hours = Math.floor((endTime / (1000 * 60 * 60)) % 24)
+        let days = Math.floor((endTime / (1000 * 60 * 60 * 24)) % 24)
+        // console.log(days + "d:" + hours + "h:" + minutes + "m:" + seconds+"s")
+        setCountDown(days + "d:" + hours + "h:" + minutes + "m:" + seconds+"s");
     }
 
-    componentDidMount() {
-        this.startTimer(this.state.fromTime);
-    }
-
-    callParentFunction(e) {
-        if (typeof this.props.onChange === 'function') {
-            this.props.onChange(e.target.value);
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.countdown}
-            </div>
-        );
-    }
+    useEffect(() => {
+        intervalId = setInterval(()=>{
+            getCountDownFromMilliseconds(endTimeInMilliseconds);
+        }, 1000)
+        // getCountDownFromMilliseconds(endTimeInMilliseconds)
+    }, [1])
+    return (
+        <div className="timer">
+            {countdown}
+        </div>
+    )
 }
 
-export default (Timer);
+
